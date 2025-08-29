460e4b6d606710ec16a5eb9d1661d76fb60279eb
Siham ElAoumari <sihamelaoumarii@gmail.com>
2025-08-29 18:03:51 +0200

    added the docs

--- chalenge1.c
+++ chalenge1.c
@@ -6,3 +6,31 @@ int main() {
     }
     return 0;
 }
+
+// login github
+
+// telecharger git  https://git-scm.com/downloads/win
+
+// setup git installation
+
+// config username and email for git  git config --global user.name "Mona Lisa", git config --global user.email "YOUR_EMAIL"
+
+
+// method 1
+// create repo "test_git " in github
+
+// create dossier in pc test_git "
+
+// inisialize local git repo in dossier  'git init'
+// create new file 
+
+// add changes to staging area  " git add ."
+
+// create commit " git commit -m "message"
+
+// connect local repo with github repo "git remote add origin url-to-put"
+
+// push changes to github repo " git push -u origin main"
+
+
+// method 2
